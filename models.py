0bbd1963e3825e728c4161694852917627924784
Jerome Swannack <jeromeswannack@gmail.com>
2023-07-12 20:25:48 +1200

    Added solution models

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
-from typing import List
+from typing import List, Tuple
 from enum import Enum
 from pydantic import BaseModel, Field
 from datetime import datetime
 
 
 # Staff related models
+
 
 class Attribute(Enum):
     DOCTOR = "doctor"
@@ -13,7 +14,9 @@ class Attribute(Enum):
     TECHNICIAN = "technician"
     RESEARCH_ASSISTANT = "research_assistant"
 
+
 SkillSet = List[Attribute]
+
 
 class Shift(BaseModel):
     # required breaks can be determined by the duration
@@ -26,22 +29,21 @@ class StaffMember(BaseModel):
     attributes: SkillSet
     shift: Shift
 
+
 # Study related models
 class Task(BaseModel):
     start_after: datetime
     finish_before: datetime
     duration: int
-
     blocked_by: List["Task"] = Field(default_factory=list)
-
     required_attributes: List[SkillSet]
-
     title: str
+
 
 class Patient(BaseModel):
     name: str
     tasks: List[Task]
-    floor_number: int
+
 
 class StudySchedule(BaseModel):
     patients: List[Patient]
@@ -55,7 +57,7 @@ class WorkDay(BaseModel):
 
 # Solution related models
 class Solution(BaseModel):
-    allocations: None
+    allocations: List[Tuple[StaffMember, Task]]
 
 
 # Other considerations
