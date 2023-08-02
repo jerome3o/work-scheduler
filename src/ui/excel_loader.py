03f265c1c60c5a7d82f6efb38d9639b2607f52eb
Olivia Kyle <oliviakyle@outlook.co.nz>
2023-08-02 12:18:44 +1200

    saved original formatting of sheet

--- src/ui/excel_loader.py
+++ src/ui/excel_loader.py
@@ -4,10 +4,14 @@ import pandas as pd
 import os
 import openpyxl
 from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.styles import NamedStyle
+import datetime
 
 
 input_file_paths = []
 input_schedule_days = []
+
 
 def create_window():
     root = tk.Tk()
@@ -52,6 +56,7 @@ def output_directory_dialog():
     output_file_path = filedialog.askdirectory()
     copy_schedule_days(input_file_paths, output_file_path, input_schedule_days)
 
+
 # Function to find rows containing the tokens
 def find_token_row_index(token, df):
     return df[df.apply(lambda row: row.astype(str).str.contains(token, case=False).any(), axis=1)].index
@@ -60,14 +65,11 @@ def find_token_row_index(token, df):
 def copy_schedule_days(input_files, output_file, day_tokens):
     # Create a new workbook for the output if it doesn't exist
     output_file_name = os.path.join(output_file, "Daily_Schedules.xlsx")
-
     if not os.path.exists(output_file_name):
         output_wb = Workbook()
         output_wb.save(output_file_name)
     else:
-        output_wb = openpyxl.load_workbook(output_file_name)
-    
-    output_sheet = output_wb.active
+        output_wb = openpyxl.load_workbook(output_file_name, values_only=True)
     
     i = 0
 
@@ -81,7 +83,6 @@ def copy_schedule_days(input_files, output_file, day_tokens):
         start_index = find_token_row_index(start_token, df)
         end_index = find_token_row_index(end_token, df)
 
-
         if not start_index.empty and not end_index.empty:
             start_index = start_index[0]
             
@@ -89,52 +90,37 @@ def copy_schedule_days(input_files, output_file, day_tokens):
             end_index = end_index[end_index > start_index][0]
 
             # Extract the section of data between the tokens (excluding the tokens themselves)
-            selected_data = df.loc[start_index -4 : end_index]
+            selected_data = df.loc[start_index - 4: end_index, df.columns != df.columns[0]]  # Exclude the first column from being copied
 
-            # Save the extracted data to a new Excel file
-            selected_data.to_excel(output_file_name, index=False)
+            # Get the selected sheet to copy data
+            sheet = output_wb.active
+
+            # Append the new data to the end of the sheet
+            for row in dataframe_to_rows(selected_data, index=False, header=False):
+                sheet.append(row)
+
+            # Define a custom style for datetime values
+            time_style = NamedStyle(name="time_style", number_format="hh:mm")
+
+            # Formatting date time
+            for row in sheet.iter_rows(min_row=sheet.max_row - len(selected_data) + 1, max_row=sheet.max_row):
+                for cell in row:
+                    source_cell = df.iloc[cell.row - sheet.max_row + len(selected_data) - 1, cell.column - 1]
+
+                     # Check if the value is a datetime object and apply the custom style
+                    if isinstance(source_cell, pd.Timestamp):
+                        cell.style = time_style
+
+            # Preserve column width
+            for column_cells in sheet.columns:
+                length = max(len(str(cell.value)) for cell in column_cells)
+                sheet.column_dimensions[column_cells[0].column_letter].width = length + 2  # Additional padding
+
+            output_wb.save(output_file_name)
+
         else:
-            print("Start and/or end tokens not found in the DataFrame.")
+            print("Day not found in study schedule")
 
-
-        """
-
-        study_day = day_tokens[i]
-
-        # Load the input workbook
-        input_wb = openpyxl.load_workbook(input_file, data_only=True)
-        input_sheet = input_wb.active
-
-        # Initialize variables to track the section copying
-        is_copying = False
-        copied_rows = 0
-
-        for row in input_sheet.iter_rows(values_only=True):
-            cell_value = row[1]  # Assuming the data to copy is in column B (index 1)
-
-            # Check if the token string is found to start copying
-            if "Day" in str(cell_value) and str(study_day) in str(cell_value):
-                is_copying = True
-                continue
-
-            # Check if we are in copying mode
-            if is_copying:
-                copied_rows += 1
-
-                # Write the row data to the output sheet
-                text_values = [str(cell) if cell is not None else "" for cell in row]
-                output_sheet.append(text_values)
-                
-
-            # Check if we are done copying after encountering the token again
-            if is_copying and "Day" in str(cell_value) and str(study_day) not in str(cell_value):
-                is_copying = False
-        i = i+1
-    
-        """
-    
-    # Save the output workbook
-    #output_wb.save(output_file_name)
     print(f"Success?")
 
 
