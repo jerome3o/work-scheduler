e7fccdd0076615902564b35dba9d6ab1cde2b5db
Jerome Swannack <jeromeswannack@gmail.com>
2023-09-13 19:24:45 +1200

    Added some stubs to implement later

---
+++ src/solver/helpers.py
@@ -0,0 +1,21 @@
+from src.models import WorkDay
+
+
+def get_task_start_time_vector(work_day: WorkDay) -> list:
+    # TODO(j.swannack): implement
+    pass
+
+
+def get_task_duration_vector(work_day: WorkDay) -> list:
+    # TODO(j.swannack): implement
+    pass
+
+
+def get_worker_start_time_vector(work_day: WorkDay) -> list:
+    # TODO(j.swannack): implement
+    pass
+
+
+def get_worker_end_time_vector(work_day: WorkDay) -> list:
+    # TODO(j.swannack): implement
+    pass
