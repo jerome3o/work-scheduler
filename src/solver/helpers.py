1a851b09875fe029b5db92a774d9e682401a9463
Jerome Swannack <jeromeswannack@gmail.com>
2023-09-13 19:25:55 +1200

    Added skillset stubs

--- src/solver/helpers.py
+++ src/solver/helpers.py
@@ -19,3 +19,15 @@ def get_worker_start_time_vector(work_day: WorkDay) -> list:
 def get_worker_end_time_vector(work_day: WorkDay) -> list:
     # TODO(j.swannack): implement
     pass
+
+
+def get_worker_skillset_matrix(work_day: WorkDay) -> list:
+    # TODO(j.swannack): implement
+    # This will be a one-hot encoding of the worker's skillset
+    # With rows/cols being workers/skills
+    pass
+
+def get_task_skillset_matrix(work_day: WorkDay) -> list:
+    # This will be a one-hot encoding of the task's required skillset
+    # With rows/cols being tasks/skills
+    pass
