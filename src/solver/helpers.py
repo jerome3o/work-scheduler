3967b82ad046bd6fec0a55aa05234bbee2669381
Jerome Swannack <jeromeswannack@gmail.com>
2023-09-14 20:22:35 +1200

    Added helper for _get_minutes_from_start_of_relative_date

--- src/solver/helpers.py
+++ src/solver/helpers.py
@@ -1,9 +1,25 @@
+from datetime import datetime
+
 from src.models import WorkDay
 
 
+def _get_minutes_from_start_of_relative_date(
+    time: datetime,
+    relative_date: datetime = None,
+) -> int:
+    if relative_date is None:
+        # get the start of the day
+        relative_date = datetime(
+            year=time.year,
+            month=time.month,
+            day=time.day,
+        )
+
+    return (time - relative_date).total_seconds() // 60
+
+
 def get_task_start_time_vector(work_day: WorkDay) -> list:
-    # TODO(j.swannack): implement
-    pass
+    return [task.time for task in work_day.tasks]
 
 
 def get_task_duration_vector(work_day: WorkDay) -> list:
@@ -26,6 +42,7 @@ def get_worker_skillset_matrix(work_day: WorkDay) -> list:
     # This will be a one-hot encoding of the worker's skillset
     # With rows/cols being workers/skills
     pass
+
 
 def get_task_skillset_matrix(work_day: WorkDay) -> list:
     # This will be a one-hot encoding of the task's required skillset
