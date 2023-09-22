02ac4d6b4b82a77d76ea5a9eaf3f7ef352530b8d
Jerome Swannack <jeromeswannack@gmail.com>
2023-09-23 09:44:26 +1200

    Added solving code, formatted

--- src/solver/formulate.py
+++ src/solver/formulate.py
@@ -6,7 +6,7 @@ from typing import Union, List
 
 import mip
 
-from models import WorkDay
+from models import WorkDay, Solution
 from solver.helpers import (
     get_task_start_time_vector,
     get_task_finish_time_vector,
@@ -50,7 +50,7 @@ def build_matrices(work_day: WorkDay) -> ModelParameters:
     )
 
 
-def build_model(
+def build_and_solve(
     work_day: WorkDay,
     model_parameters: ModelParameters,
 ) -> mip.Model:
@@ -81,20 +81,21 @@ def build_model(
     # Workers can only work on tasks that start after their start time
     for i, j in product(range(n_workers), range(n_tasks)):
         model += (
-            worker_task_matrix[i][j]*model_parameters.worker_start_vector[i]
-            - worker_task_matrix[i][j]*model_parameters.task_start_vector[j] <= 0
+            worker_task_matrix[i][j] * model_parameters.worker_start_vector[i]
+            - worker_task_matrix[i][j] * model_parameters.task_start_vector[j]
+            <= 0
         )
 
     # Workers can only work on tasks that finish before their end time
     for i, j in product(range(n_workers), range(n_tasks)):
         model += (
-            worker_task_matrix[i][j]*model_parameters.task_finish_vector[j]
-            - worker_task_matrix[i][j]*model_parameters.worker_finish_vector[i] <= 0
+            worker_task_matrix[i][j] * model_parameters.task_finish_vector[j]
+            - worker_task_matrix[i][j] * model_parameters.worker_finish_vector[i]
+            <= 0
         )
 
     # Workers need breaks (logic tbd, maybe ignore for prototype)
     # Workers can only work on tasks that they have the skills for
-
 
     # Objective
 
@@ -110,6 +111,22 @@ def build_model(
 
     # TODO(j.swannack): Minimize high skilled workers doing "any" task
 
+    model.optimize()
+
+    allocations = []
+
+    for i in range(n_workers):
+        for j in range(n_tasks):
+            if worker_task_matrix[i][j].x >= 0.99:
+                allocations.append((work_day.staff_members[i], work_day.tasks[j]))
+
+    return Solution(allocations=allocations)
+
+
+def solve(work_day: WorkDay) -> Solution:
+    model_parameters = build_matrices(work_day)
+    return build_and_solve(work_day, model_parameters)
+
 
 def main():
     # todo define relative day somewhere in the workday
@@ -123,7 +140,9 @@ def main():
     for data_file in data_files:
         work_day = load_work_day(data_file)
         work_day = _remove_tasks_with_no_time(work_day)
-        params = build_matrices(work_day)
+        solution = solve(work_day)
+        output_file = Path(data_file).with_suffix(".out.json")
+        Path(output_file).write_text(solution.json(indent=2))
 
 
 if __name__ == "__main__":
