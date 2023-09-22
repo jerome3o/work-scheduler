b748c4b27bc05708d6cde7b106ac6d3b985a541a
Jerome Swannack <jeromeswannack@gmail.com>
2023-09-23 09:35:11 +1200

    Added first draft of build_model

--- src/solver/formulate.py
+++ src/solver/formulate.py
@@ -1,5 +1,10 @@
+from itertools import product
+
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Union
+from typing import Union, List
+
+import mip
 
 from models import WorkDay
 from solver.helpers import (
@@ -12,6 +17,16 @@ from solver.helpers import (
 )
 
 
+@dataclass
+class ModelParameters:
+    task_start_vector: List[int]
+    task_finish_vector: List[int]
+    task_skillset_matrix: List[int]
+    worker_start_vector: List[int]
+    worker_finish_vector: List[int]
+    worker_skillset_matrix: List[int]
+
+
 def load_work_day(file: Union[str, Path]) -> WorkDay:
     return WorkDay.parse_file(file)
 
@@ -19,36 +34,81 @@ def load_work_day(file: Union[str, Path]) -> WorkDay:
 def _remove_tasks_with_no_time(workday: WorkDay) -> WorkDay:
     # This is a work around while we still have tasks with no time
     return WorkDay(
-        tasks=[task for task in workday.tasks if task.time is not ''],
+        tasks=[task for task in workday.tasks if task.time is not ""],
         staff_members=workday.staff_members,
     )
 
 
-def build_matrices(work_day: WorkDay):
-    task_start_vector = get_task_start_time_vector(work_day)
-    task_duration_vector = get_task_finish_time_vector(work_day)
-    worker_start_vector = get_worker_start_time_vector(work_day)
-    worker_end_vector = get_worker_finish_time_vector(work_day)
-
-    worker_skillset_matrix = get_worker_skillset_matrix(work_day)
-    task_skillset_matrix = get_task_skillset_matrix(work_day)
-
-    # print them all
-    print(
-        f"task_start_vector: {task_start_vector}\n"
-        f"task_duration_vector: {task_duration_vector}\n"
-        f"worker_start_vector: {worker_start_vector}\n"
-        f"worker_end_vector: {worker_end_vector}\n"
-        f"worker_skillset_matrix: {worker_skillset_matrix}\n"
-        f"task_skillset_matrix: {task_skillset_matrix}\n"
+def build_matrices(work_day: WorkDay) -> ModelParameters:
+    return ModelParameters(
+        task_start_vector=get_task_start_time_vector(work_day),
+        task_finish_vector=get_task_finish_time_vector(work_day),
+        worker_start_vector=get_worker_start_time_vector(work_day),
+        worker_finish_vector=get_worker_finish_time_vector(work_day),
+        worker_skillset_matrix=get_worker_skillset_matrix(work_day),
+        task_skillset_matrix=get_task_skillset_matrix(work_day),
     )
 
-    return (
-        task_start_vector,
-        task_duration_vector,
-        worker_start_vector,
-        worker_end_vector,
-    )
+
+def build_model(
+    work_day: WorkDay,
+    model_parameters: ModelParameters,
+) -> mip.Model:
+    model = mip.Model("work-scheduler")
+
+    n_workers = len(work_day.staff_members)
+    n_tasks = len(work_day.tasks)
+
+    # worker x task matrix
+    worker_task_matrix = [
+        [model.add_var(var_type=mip.BINARY) for _ in work_day.tasks]
+        for _ in work_day.staff_members
+    ]
+
+    # task x worker matrix
+    task_worker_matrix = list(zip(*worker_task_matrix))
+
+    # Constraints
+
+    # Workers can only work on one task at a time
+    for i in range(n_workers):
+        model += mip.xsum(worker_task_matrix[i]) <= 1
+
+    # Tasks can only have one worker assigned to them
+    for i in range(n_tasks):
+        model += mip.xsum(task_worker_matrix[i]) <= 1
+
+    # Workers can only work on tasks that start after their start time
+    for i, j in product(range(n_workers), range(n_tasks)):
+        model += (
+            worker_task_matrix[i][j]*model_parameters.worker_start_vector[i]
+            - worker_task_matrix[i][j]*model_parameters.task_start_vector[j] <= 0
+        )
+
+    # Workers can only work on tasks that finish before their end time
+    for i, j in product(range(n_workers), range(n_tasks)):
+        model += (
+            worker_task_matrix[i][j]*model_parameters.task_finish_vector[j]
+            - worker_task_matrix[i][j]*model_parameters.worker_finish_vector[i] <= 0
+        )
+
+    # Workers need breaks (logic tbd, maybe ignore for prototype)
+    # Workers can only work on tasks that they have the skills for
+
+
+    # Objective
+
+    # Evenly distribute tasks
+    lowest_workload = model.add_var(name="lowest_workload")
+    highest_workload = model.add_var(name="highest_workload")
+
+    for i in range(n_workers):
+        model += mip.xsum(worker_task_matrix[i]) <= highest_workload
+        model += mip.xsum(worker_task_matrix[i]) >= lowest_workload
+
+    model.objective = mip.minimize(highest_workload - lowest_workload)
+
+    # TODO(j.swannack): Minimize high skilled workers doing "any" task
 
 
 def main():
@@ -63,7 +123,7 @@ def main():
     for data_file in data_files:
         work_day = load_work_day(data_file)
         work_day = _remove_tasks_with_no_time(work_day)
-        build_matrices(work_day)
+        params = build_matrices(work_day)
 
 
 if __name__ == "__main__":
