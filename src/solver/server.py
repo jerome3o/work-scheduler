c9cc0b6f9b48746a93ac3380c909a571f65fc271
Jerome Swannack <jeromeswannack@gmail.com>
2023-11-27 22:33:02 +1300

    Added run_solver call in /solve endpoint, and static server for results

--- src/solver/server.py
+++ src/solver/server.py
@@ -1,14 +1,27 @@
-from fastapi import FastAPI
+# runs solver on /solve endpoint
+# statically serves the output directory
 
-from models import Solution, SolverOutput, SolverInput
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from models import Solution, WorkDay
+from solver.pipeline import run_solver
 
 
 app = FastAPI()
 
 
+app.mount("/outputs", app=StaticFiles(directory="outputs"), name="outputs")
+
+
 @app.get("/solve")
-def solve(solver_input: SolverInput) -> SolverOutput:
-    pass
+def solve(work_day: WorkDay) -> Solution:
+    return run_solver(
+        work_day=work_day,
+        # todo: use a guid for each solution
+        output_dir="outputs/example_report",
+        open_browser=False,
+    )
 
 
 if __name__ == "__main__":
