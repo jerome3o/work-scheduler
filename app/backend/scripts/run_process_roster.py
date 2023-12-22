998edb5ff25ddc264383f6b007bc14f27d41d6ad
Jerome Swannack <jeromeswannack@gmail.com>
2023-12-23 11:24:05 +1300

    Set up test fixture for roster processing

--- app/backend/scripts/run_process_roster.py
+++ app/backend/scripts/run_process_roster.py
@@ -1,1 +1,23 @@
-print(f"Running {__file__}")
+# import TestClient from fastapi.testclient
+
+from fastapi.testclient import TestClient
+
+from scheduler.server import app
+
+_ROSTER_FILE = "scratch/ui/Duty Roster 13 June - 26 June 22.xlsx"
+
+
+def main():
+    client = TestClient(app)
+    response = client.post(
+        "/api/process-files/roster",
+        files={"roster_file": open(_ROSTER_FILE, "rb")},
+    )
+    print(response.json())
+
+
+if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(level=logging.INFO)
+    main()
